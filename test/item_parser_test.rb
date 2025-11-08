fc048d418f7415665a33313f2b8d725a7a93dee8
Alex van den Berg <alexvdberg010109@gmail.com>
2025-11-08 21:25:06 +0200

    test: add new test for item parser to work with resonator items

--- test/item_parser_test.rb
+++ test/item_parser_test.rb
@@ -6,6 +6,7 @@ require 'item_parser'
 class ItemParserTest < Minitest::Test
   def setup
     @fossil_input = "Item Class: Stackable Currency\nRarity: Currency\nShuddering Fossil\n--------\nStack Size: 2/20\n--------\nMore Speed modifiers\nNo Mana modifiers\n--------\nPlace in a Resonator to influence item crafting.\nShift click to unstack."
+    @resonator_input = "Item Class: Delve Stackable Socketable Currency\nRarity: Currency\nPrime Chaotic Resonator\n--------\nStack Size: 2/10\nRequires 4 (unmet) Socketed Fossils\n--------\nSockets: D D D D\n--------\nReforges a rare item with new random modifiers\n--------\nAll sockets must be filled with Fossils before this item can be used.\nShift click to unstack."
   end
 
   def test_parse_sections_of_input_fossil
@@ -16,6 +17,17 @@ class ItemParserTest < Minitest::Test
                    stack_size: '2/20',
                    modifiers: ['More Speed modifiers', 'No Mana modifiers'],
                    description: 'Place in a Resonator to influence item crafting.'
+                 })
+  end
+
+  def test_parse_sections_of_input_resonator
+    assert_equal(ItemParser.parse(@resonator_input), {
+                   item_class: 'Delve Stackable Socketable Currency',
+                   rarity: 'Currency',
+                   name: 'Prime Chaotic Resonator',
+                   stack_size: '2/10',
+                   modifiers: ['Reforges a rare item with new random modifiers'],
+                   description: 'All sockets must be filled with Fossils before this item can be used.Shift click to unstack.'
                  })
   end
 end
