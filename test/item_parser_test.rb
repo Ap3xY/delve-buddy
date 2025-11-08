dc5fff03148e013ad77299ae8f7f2de6c30d3f16
Alex van den Berg <alexvdberg010109@gmail.com>
2025-11-08 19:24:30 +0200

    refactor: move item parser test input to a instance variable
    refactor: change item parser test input variable to input_fossil

--- test/item_parser_test.rb
+++ test/item_parser_test.rb
@@ -4,9 +4,12 @@ require 'minitest/autorun'
 require 'item_parser'
 
 class ItemParserTest < Minitest::Test
+  def setup
+    @fossil_input = "Item Class: Stackable Currency\nRarity: Currency\nShuddering Fossil\n--------\nStack Size: 2/20\n--------\nMore Speed modifiers\nNo Mana modifiers\n--------\nPlace in a Resonator to influence item crafting.\nShift click to unstack."
+  end
+
   def test_parse_first_section_of_input_fossil
-    input = "Item Class: Stackable Currency\nRarity: Currency\nShuddering Fossil\n--------\nStack Size: 2/20\n--------\nMore Speed modifiers\nNo Mana modifiers\n--------\nPlace in a Resonator to influence item crafting.\nShift click to unstack."
     assert_equal({ item_class: 'Stackable Currency', rarity: 'Currency', name: 'Shuddering Fossil' },
-                 ItemParser.parse(input))
+                 ItemParser.parse(@fossil_input))
   end
 end
